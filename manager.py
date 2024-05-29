04370e9f2e6ab7fcf8f6b0af8bc7923917a90408
Amir ali Maroufi <128319452+amiralimaroufi82@users.noreply.github.com>
2024-05-29 07:40:51 +0330

    manager.py

--- manager.py
+++ manager.py
@@ -1,29 +1,56 @@
-class User:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+import json
+import os
+import re
+import logging
+from project_manager import ProjectManager
+logging.basicConfig(filename='project.log', level=logging.INFO,format='%(asctime)s - %(levelname)s - %(message)s')
+    
+    
+def is_valid_email(email):
+    email_regex = re.compile(r"^[\w\.-]+@[\w\.-]+\.\w+$")
+    return re.match(email_regex, email) is not None
 
-class Task:
-    def __init__(self, description):
-        self.description = description
+def create_admin(file_path='admin_data.json'):
+    username = input("Enter username for the admin user: ")
+    password = input("Enter password for the admin user: ")
+    email = input("Enter email for the admin user: ")
 
-class Project:
-    def __init__(self, name):
-        self.name = name
-        self.tasks = []
+    if not is_valid_email(email):
+        print("Invalid email address")
+        return
 
-class ProjectManager:
-    def __init__(self):
-        self.projects = []
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            if data.get('username') == username:
+                print(f"Error: Admin with username '{username}' already exists.")
+                return
+    else:
+        data = {}
 
-    def create_project(self, name):
-        project = Project(name)
-        self.projects.append(project)
+    data['username'] = username
+    data['password'] = password
+    data['email'] = email
+    try:
+        with open(file_path, 'w') as file:
+            json.dump(data, file, indent=4)
+        print(f"Admin '{username}' created successfully.")
+        logging.info("Admin '%s' created successfully.", username)
+    except Exception as e:
+        logging.error("Error saving admin data: %s", str(e))
+        
+def delete_all(self):
+        pr= ProjectManager()
+        confirmation = input("Are you sure you want to delete all data? (yes/no): ")
+        if confirmation.lower() == "yes":
+            pr.users = {}
+            pr.projects = {}
+            print("All data has been deleted.")
+        else:
+            print("Operation canceled.")
+            
+def main():
+    create_admin()
 
-    def add_task_to_project(self, project, task_description):
-        task = Task(task_description)
-        project.tasks.append(task)
-
-
-
-
+if __name__ == '__main__':
+    main()
